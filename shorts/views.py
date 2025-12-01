10b49a4791563e1608d033aba6809dddb44e3346
Loris Rovitti <190294974+LorisCodex@users.noreply.github.com>
2025-12-01 17:26:08 +0100

    Validate Short existence in ShortStatsViewSet queryset
    Added get_object_or_404 lookup for Short in get_queryset to ensure
    a 404 response when the parent Short does not exist.

--- shorts/views.py
+++ shorts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, viewsets
+from django.shortcuts import get_object_or_404
 
 from shorts.models import Short, ShortStats
 from shorts.serializers import ShortSerializer, ShortStatsSerializer
@@ -10,3 +11,7 @@ class ShortViewSet(viewsets.ModelViewSet):
 class ShortStatsViewSet(viewsets.ModelViewSet):
     queryset = ShortStats.objects.all()
     serializer_class = ShortStatsSerializer
+    
+    def get_queryset(self):
+        short = get_object_or_404(Short, pk=self.kwargs['short_pk'])
+        return ShortStats.objects.filter(short=short)
