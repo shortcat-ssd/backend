65b5134c20b9a69cdc507e49d05a556e31694021
Loris Rovitti <190294974+LorisCodex@users.noreply.github.com>
2025-12-18 09:41:12 +0100

    Update settings.py

--- shortcat/settings.py
+++ shortcat/settings.py
@@ -159,6 +159,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
 
 SITE_ID = 1
 
